38cd48943f0ceca99f48b80876fdd0c28517df2a
Evans Frimpong <ezfrimps@gmail.com>
2021-10-06 12:56:36 -0400

    complete setup working with firewalls

--- playbooks/web/index.php
+++ playbooks/web/index.php
@@ -2,13 +2,13 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 echo "Server IP Address is: $ip_server";
 
-$servername = "192.168.3.12";
-$username = "example_user";
-$password = "secret";
+$servername = "10.100.2.12";
+$username = "devops_user";
+$password = "password";
 echo "<br>";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=example_db", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=devops_db", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully to mysql database ip address $servername";
@@ -28,7 +28,7 @@ try {
     <?php $date = new DateTime();
     echo $date->format('Y-m-d H:i:s'); ?>
     <br>
-    <img src="https://cdn0.iconfinder.com/data/icons/nature-food-and-kitchen/1000/file_light-43-512.png" alt="Google logo"
+    <img src="https://cdn0.iconfinder.com/data/icons/nature-food-and-kitchen/1000/file_light-43-512.png" alt="boobs"
         width="200">
     <br>
  <?php
